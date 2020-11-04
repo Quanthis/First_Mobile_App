e01a4e4c0ffe3ecfaebca197c41531886834746c
Młyński Stanisław <mlynski.stanislaw@gmail.com>
2020-11-04 13:01:03 +0100

    Content --Added content pages for all optics

--- MaybeThisWillWork/MaybeThisWillWork/SubNavigationPage_Sights.xaml.cs
+++ MaybeThisWillWork/MaybeThisWillWork/SubNavigationPage_Sights.xaml.cs
@@ -13,9 +13,53 @@ namespace MaybeThisWillWork
             InitializeComponent();
         }
 
+        private async void MoveToDigitalx1(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new OpticsContentPages.Digitalx1());
+        }
+
         private async void MoveToClassicx1(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new OpticsContentPages.Classicx1());
+        }
+
+        private async void MoveToClassicx2(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new OpticsContentPages.Classicx2());
+        }
+
+        private async void MoveToHolox1(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new OpticsContentPages.Holox1());
+        }
+
+        private async void MoveToHolox2(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new OpticsContentPages.Holox2());
+        }
+
+        private async void MoveToRangerx3(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new OpticsContentPages.Rangerx3());
+        }
+        private async void MoveToAOGx2_4(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new OpticsContentPages.Classicx1());
+        }
+
+        private async void MoveToSniperx6(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new OpticsContentPages.Sniperx6());
+        }
+
+        private async void MoveToSniperx4_8(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new OpticsContentPages.Sniperx4_10());
+        }
+
+        private async void MoveToSniperx4_10(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new OpticsContentPages.Digitalx4_10());
         }
     }
 }
